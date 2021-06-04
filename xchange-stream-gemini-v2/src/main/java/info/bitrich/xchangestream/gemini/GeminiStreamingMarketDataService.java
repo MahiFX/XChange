ba6a7ee3fb41c0d44a1d25df10ff9cdbff61ce5a
James Furness <mahijames@users.noreply.github.com>
2021-06-04 17:29:15 +0100

    Clear state and publish empty book on disconnect

--- xchange-stream-gemini-v2/src/main/java/info/bitrich/xchangestream/gemini/GeminiStreamingMarketDataService.java
+++ xchange-stream-gemini-v2/src/main/java/info/bitrich/xchangestream/gemini/GeminiStreamingMarketDataService.java
@@ -3,19 +3,25 @@ package info.bitrich.xchangestream.gemini;
 import com.google.common.base.MoreObjects;
 import info.bitrich.xchangestream.core.StreamingMarketDataService;
 import io.reactivex.Observable;
-import java.math.BigDecimal;
-import java.util.Map;
-import java.util.SortedMap;
-import java.util.TreeMap;
-import java.util.concurrent.ConcurrentHashMap;
 import org.knowm.xchange.currency.CurrencyPair;
 import org.knowm.xchange.dto.marketdata.OrderBook;
 import org.knowm.xchange.dto.marketdata.Ticker;
 import org.knowm.xchange.dto.marketdata.Trade;
 import org.knowm.xchange.exceptions.NotYetImplementedForExchangeException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.math.BigDecimal;
+import java.util.Collections;
+import java.util.Date;
+import java.util.Map;
+import java.util.SortedMap;
+import java.util.TreeMap;
+import java.util.concurrent.ConcurrentHashMap;
 
 /** Adapted from V1 by Max Gao on 01-09-2021 */
 public class GeminiStreamingMarketDataService implements StreamingMarketDataService {
+  private static final Logger LOG = LoggerFactory.getLogger(GeminiStreamingMarketDataService.class);
   private static final String L2_UPDATES = "l2_updates";
   private final GeminiStreamingService service;
 
@@ -38,17 +44,31 @@ public class GeminiStreamingMarketDataService implements StreamingMarketDataServ
 
     int maxDepth = (int) MoreObjects.firstNonNull(args.length > 0 ? args[0] : null, 1);
 
-    return service
-        .getRawWebSocketTransactions(currencyPair, false)
-        .filter(message -> (L2_UPDATES).equals(message.getType()))
-        .map(
-            message -> {
-              bids.computeIfAbsent(
-                  currencyPair, k -> new TreeMap<>(java.util.Collections.reverseOrder()));
-              asks.computeIfAbsent(currencyPair, k -> new TreeMap<>());
-              return message.toOrderBook(
-                  bids.get(currencyPair), asks.get(currencyPair), maxDepth, currencyPair);
-            });
+    Observable<OrderBook> disconnectStream = service.subscribeDisconnect().map(
+            o -> {
+              LOG.warn("Invalidating book due to disconnect {}", o);
+              bids.clear();
+              asks.clear();
+              return new OrderBook(new Date(), Collections.emptyList(), Collections.emptyList());
+            }
+    );
+
+    Observable<OrderBook> orderBookStream = service
+            .getRawWebSocketTransactions(currencyPair, false)
+            .filter(message -> (L2_UPDATES).equals(message.getType()))
+            .map(
+                    message -> {
+                      bids.computeIfAbsent(
+                              currencyPair, k -> new TreeMap<>(Collections.reverseOrder()));
+                      asks.computeIfAbsent(currencyPair, k -> new TreeMap<>());
+                      return message.toOrderBook(
+                              bids.get(currencyPair), asks.get(currencyPair), maxDepth, currencyPair);
+                    });
+
+    return Observable.merge(
+            orderBookStream,
+            disconnectStream
+    );
   }
 
   @Override
