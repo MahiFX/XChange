1f9ad0552c871d4886c1c3a1e980fef13a0ab3cd
James Furness <mahijames@users.noreply.github.com>
2021-06-08 14:34:44 +0100

    Bitfinex receivedTimestamp

--- xchange-bitfinex/src/main/java/org/knowm/xchange/bitfinex/v1/dto/marketdata/BitfinexLevel.java
+++ xchange-bitfinex/src/main/java/org/knowm/xchange/bitfinex/v1/dto/marketdata/BitfinexLevel.java
@@ -1,13 +1,21 @@
 package org.knowm.xchange.bitfinex.v1.dto.marketdata;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
+
 import java.math.BigDecimal;
+import java.util.Date;
 
 public class BitfinexLevel {
 
   private final BigDecimal price;
   private final BigDecimal amount;
   private final BigDecimal timestamp;
+
+  /** Local timestamp at which this event was received */
+  @JsonIgnore
+  private transient final Date receivedTimestamp;
 
   /**
    * Constructor
@@ -16,14 +24,32 @@ public class BitfinexLevel {
    * @param amount
    * @param timestamp
    */
+  @JsonCreator
   public BitfinexLevel(
-      @JsonProperty("price") BigDecimal price,
-      @JsonProperty("amount") BigDecimal amount,
-      @JsonProperty("timestamp") BigDecimal timestamp) {
+          @JsonProperty("price") BigDecimal price,
+          @JsonProperty("amount") BigDecimal amount,
+          @JsonProperty("timestamp") BigDecimal timestamp) {
+    this(price, amount, timestamp, new Date());
+  }
+
+  /**
+   * Constructor
+   *
+   * @param price
+   * @param amount
+   * @param timestamp
+   * @param receivedTimestamp
+   */
+  public BitfinexLevel(
+          BigDecimal price,
+          BigDecimal amount,
+          BigDecimal timestamp,
+          Date receivedTimestamp) {
 
     this.price = price;
     this.amount = amount;
     this.timestamp = timestamp;
+    this.receivedTimestamp = receivedTimestamp;
   }
 
   public BigDecimal getPrice() {
@@ -39,6 +65,11 @@ public class BitfinexLevel {
   public BigDecimal getTimestamp() {
 
     return timestamp;
+  }
+
+  /** Local timestamp at which this event was received */
+  public Date getReceivedTimestamp() {
+    return receivedTimestamp;
   }
 
   @Override
