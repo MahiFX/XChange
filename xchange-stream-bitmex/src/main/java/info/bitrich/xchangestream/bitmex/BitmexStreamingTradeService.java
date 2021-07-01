42f111659733d5767a44f75e42d686994cbc5b6d
Liam Cordelle <liam.cordelle@mahifx.com>
2021-07-01 17:17:12 +1200

    BitMEX - Fix orderChanges to work for multiple instruments

--- xchange-stream-bitmex/src/main/java/info/bitrich/xchangestream/bitmex/BitmexStreamingTradeService.java
+++ xchange-stream-bitmex/src/main/java/info/bitrich/xchangestream/bitmex/BitmexStreamingTradeService.java
@@ -3,17 +3,23 @@ package info.bitrich.xchangestream.bitmex;
 import info.bitrich.xchangestream.bitmex.dto.BitmexOrder;
 import info.bitrich.xchangestream.core.StreamingTradeService;
 import io.reactivex.Observable;
-
-import java.util.Arrays;
-import java.util.stream.Collectors;
-
+import io.reactivex.subjects.PublishSubject;
+import io.reactivex.subjects.Subject;
 import org.knowm.xchange.currency.CurrencyPair;
 import org.knowm.xchange.dto.Order;
+
+import java.util.Arrays;
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.stream.Collectors;
 
 /**
  * Created by Declan
  */
 public class BitmexStreamingTradeService implements StreamingTradeService {
+    private static final String ORDER_CHANGES_CHANNEL_NAME = "order";
+
+    private final Subject<BitmexOrder> orderChangesPublisher = PublishSubject.<BitmexOrder>create().toSerialized();
+    private final AtomicBoolean subscribed = new AtomicBoolean(false);
 
     private final BitmexStreamingService streamingService;
 
@@ -21,20 +27,27 @@ public class BitmexStreamingTradeService implements StreamingTradeService {
         this.streamingService = streamingService;
     }
 
-    @Override
-    public Observable<Order> getOrderChanges(CurrencyPair currencyPair, Object... args) {
-        String channelName = "order";
-        String instrument = currencyPair.base.toString() + currencyPair.counter.toString();
-        return streamingService
-                .subscribeBitmexChannel(channelName)
+    public void startSubscription() {
+        streamingService.subscribeBitmexChannel(ORDER_CHANGES_CHANNEL_NAME)
                 .flatMapIterable(
                         s -> {
                             BitmexOrder[] bitmexOrders = s.toBitmexOrders();
                             return Arrays.stream(bitmexOrders)
-                                    .filter(bitmexOrder -> bitmexOrder.getSymbol().equals(instrument))
                                     .filter(BitmexOrder::isNotWorkingIndicator)
-                                    .map(BitmexOrder::toOrder)
                                     .collect(Collectors.toList());
-                        });
+                        })
+                .subscribe(orderChangesPublisher::onNext);
+    }
+
+    @Override
+    public Observable<Order> getOrderChanges(CurrencyPair currencyPair, Object... args) {
+        if (subscribed.getAndSet(true)) {
+            startSubscription();
+        }
+
+        String instrument = currencyPair.base.toString() + currencyPair.counter.toString();
+        return orderChangesPublisher
+                .filter(bitmexOrder -> bitmexOrder.getSymbol().equals(instrument))
+                .map(BitmexOrder::toOrder);
     }
 }
