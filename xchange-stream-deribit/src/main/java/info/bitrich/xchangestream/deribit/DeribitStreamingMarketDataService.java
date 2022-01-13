33fb9e63463e46477b8be43123e9605889771648
Liam Cordelle <liam.cordelle@mahifx.com>
2022-01-14 11:39:59 +1300

    Deribit Streaming - MD trade feed

--- xchange-stream-deribit/src/main/java/info/bitrich/xchangestream/deribit/DeribitStreamingMarketDataService.java
+++ xchange-stream-deribit/src/main/java/info/bitrich/xchangestream/deribit/DeribitStreamingMarketDataService.java
@@ -1,9 +1,11 @@
 package info.bitrich.xchangestream.deribit;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import info.bitrich.xchangestream.core.StreamingMarketDataService;
 import info.bitrich.xchangestream.deribit.dto.DeribitMarketDataUpdateMessage;
+import info.bitrich.xchangestream.deribit.dto.DeribitTradeData;
 import info.bitrich.xchangestream.service.netty.StreamingObjectMapperHelper;
 import io.reactivex.Observable;
 import io.reactivex.disposables.Disposable;
@@ -15,6 +17,8 @@ import org.knowm.xchange.exceptions.NotAvailableFromExchangeException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
@@ -43,7 +47,7 @@ public class DeribitStreamingMarketDataService implements StreamingMarketDataSer
     }
 
     private void setupOrderBookSubscriptions(CurrencyPair currencyPair, DeribitOrderBook orderBook) {
-        String channelName = "book." + currencyPair.toString().replace("/", "-") + ".raw";
+        String channelName = "book." + instrumentName(currencyPair) + ".raw";
 
         logger.debug("Subscribing to orderBook channel: " + channelName);
 
@@ -58,16 +62,13 @@ public class DeribitStreamingMarketDataService implements StreamingMarketDataSer
 
         Observable<DeribitMarketDataUpdateMessage> marketDataUpdateMessageObservable = streamingService.subscribeChannel(channelName)
                 .map(json -> {
-                    if (json.has("params")) {
-                        JsonNode params = json.get("params");
-                        if (params.has("data")) {
-                            JsonNode data = params.get("data");
+                    DeribitMarketDataUpdateMessage marketDataUpdate = tryGetDataAsType(json, DeribitMarketDataUpdateMessage.class);
 
-                            return mapper.treeToValue(data, DeribitMarketDataUpdateMessage.class);
-                        }
+                    if (marketDataUpdate != null) {
+                        return marketDataUpdate;
+                    } else {
+                        return DeribitMarketDataUpdateMessage.EMPTY;
                     }
-
-                    return DeribitMarketDataUpdateMessage.EMPTY;
                 });
 
         Disposable orderBookSubscriptionDisposable = marketDataUpdateMessageObservable
@@ -96,6 +97,24 @@ public class DeribitStreamingMarketDataService implements StreamingMarketDataSer
         });
     }
 
+    private <T> T tryGetDataAsType(JsonNode json, Class<T> dataType) throws JsonProcessingException {
+        if (json.has("params")) {
+            JsonNode params = json.get("params");
+
+            if (params.has("data")) {
+                JsonNode data = params.get("data");
+
+                return mapper.treeToValue(data, dataType);
+            }
+        }
+
+        return null;
+    }
+
+    private String instrumentName(CurrencyPair currencyPair) {
+        return currencyPair.toString().replace("/", "-");
+    }
+
     private void closeAndReconnectOrderBook(CurrencyPair instrument, DeribitOrderBook orderBook, Disposable... disposables) {
         // Dispose disposables
         for (Disposable disposable : disposables) {
@@ -118,6 +137,29 @@ public class DeribitStreamingMarketDataService implements StreamingMarketDataSer
 
     @Override
     public Observable<Trade> getTrades(CurrencyPair currencyPair, Object... args) {
-        throw new NotAvailableFromExchangeException();
+        String channelName = "trades." + instrumentName(currencyPair) + ".raw";
+
+        logger.debug("Subscribing to trade channel: " + channelName);
+
+        Observable<DeribitTradeData[]> tradeData = streamingService.subscribeChannel(channelName)
+                .map(json -> {
+                    DeribitTradeData[] deribitTradeData = tryGetDataAsType(json, DeribitTradeData[].class);
+
+                    if (deribitTradeData != null) {
+                        return deribitTradeData;
+                    } else {
+                        return new DeribitTradeData[0];
+                    }
+                });
+
+        return tradeData.flatMapIterable(dtdArr -> {
+            List<Trade> trades = new ArrayList<>();
+
+            for (DeribitTradeData deribitTradeData : dtdArr) {
+                trades.add(deribitTradeData.toTrade(currencyPair));
+            }
+
+            return trades;
+        });
     }
 }
---
+++ xchange-stream-deribit/src/main/java/info/bitrich/xchangestream/deribit/dto/DeribitTradeData.java
@@ -0,0 +1,134 @@
+package info.bitrich.xchangestream.deribit.dto;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import org.knowm.xchange.currency.CurrencyPair;
+import org.knowm.xchange.dto.Order;
+import org.knowm.xchange.dto.marketdata.Trade;
+
+import java.math.BigDecimal;
+import java.util.Date;
+
+public class DeribitTradeData {
+    public final BigDecimal amount;
+    public final String blockTradeId;
+    public final String direction;
+    public final BigDecimal indexPrice;
+    public final String instrumentName;
+    public final BigDecimal iv;
+    public final String liquidation;
+    public final BigDecimal markPrice;
+    public final BigDecimal price;
+    public final Integer tickDirection;
+    public final Date timestamp;
+    public final String tradeId;
+    public final Integer tradeSeq;
+
+    public DeribitTradeData(
+            @JsonProperty("amount") BigDecimal amount,
+            @JsonProperty("block_trade_id") String blockTradeId,
+            @JsonProperty("direction") String direction,
+            @JsonProperty("index_price") BigDecimal indexPrice,
+            @JsonProperty("instrument_name") String instrumentName,
+            @JsonProperty("iv") BigDecimal iv,
+            @JsonProperty("liquidation") String liquidation,
+            @JsonProperty("mark_price") BigDecimal markPrice,
+            @JsonProperty("price") BigDecimal price,
+            @JsonProperty("tick_direction") Integer tickDirection,
+            @JsonProperty("timestamp") Date timestamp,
+            @JsonProperty("trade_id") String tradeId,
+            @JsonProperty("trade_seq") Integer tradeSeq) {
+        this.amount = amount;
+        this.blockTradeId = blockTradeId;
+        this.direction = direction;
+        this.indexPrice = indexPrice;
+        this.instrumentName = instrumentName;
+        this.iv = iv;
+        this.liquidation = liquidation;
+        this.markPrice = markPrice;
+        this.price = price;
+        this.tickDirection = tickDirection;
+        this.timestamp = timestamp;
+        this.tradeId = tradeId;
+        this.tradeSeq = tradeSeq;
+    }
+
+    @JsonProperty("amount")
+    public BigDecimal getAmount() {
+        return amount;
+    }
+
+    @JsonProperty("block_trade_id")
+    public String getBlockTradeId() {
+        return blockTradeId;
+    }
+
+    @JsonProperty("direction")
+    public String getDirection() {
+        return direction;
+    }
+
+    @JsonProperty("index_price")
+    public BigDecimal getIndexPrice() {
+        return indexPrice;
+    }
+
+    @JsonProperty("instrument_name")
+    public String getInstrumentName() {
+        return instrumentName;
+    }
+
+    @JsonProperty("iv")
+    public BigDecimal getIv() {
+        return iv;
+    }
+
+    @JsonProperty("liquidation")
+    public String getLiquidation() {
+        return liquidation;
+    }
+
+    @JsonProperty("mark_price")
+    public BigDecimal getMarkPrice() {
+        return markPrice;
+    }
+
+    @JsonProperty("price")
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    @JsonProperty("tick_direction")
+    public Integer getTickDirection() {
+        return tickDirection;
+    }
+
+    @JsonProperty("timestamp")
+    public Date getTimestamp() {
+        return timestamp;
+    }
+
+    @JsonProperty("trade_id")
+    public String getTradeId() {
+        return tradeId;
+    }
+
+    @JsonProperty("trade_seq")
+    public Integer getTradeSeq() {
+        return tradeSeq;
+    }
+
+    @JsonIgnore
+    public Trade toTrade(CurrencyPair currencyPair) {
+        return new Trade(
+                "Buy".equals(direction) ? Order.OrderType.BID : Order.OrderType.ASK,
+                amount,
+                currencyPair,
+                price,
+                timestamp,
+                tradeId,
+                null,
+                null
+        );
+    }
+}
