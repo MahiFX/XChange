d00f712b0be1a003f6b21ba53ed52944cc33637a
Liam Cordelle <liam.cordelle@mahifx.com>
2022-01-26 16:09:21 +1300

    Deribit Streaming - Fix timeInForce

--- xchange-stream-deribit/src/main/java/info/bitrich/xchangestream/deribit/dto/DeribitTimeInForce.java
+++ xchange-stream-deribit/src/main/java/info/bitrich/xchangestream/deribit/dto/DeribitTimeInForce.java
@@ -1,8 +1,10 @@
 package info.bitrich.xchangestream.deribit.dto;
 
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
 import com.fasterxml.jackson.annotation.JsonValue;
 import org.knowm.xchange.dto.Order;
 
+@JsonTypeInfo(use = JsonTypeInfo.Id.NONE)
 public enum DeribitTimeInForce implements Order.IOrderFlags {
     GTC("good_til_cancelled"),
     GTD("good_til_day"),
