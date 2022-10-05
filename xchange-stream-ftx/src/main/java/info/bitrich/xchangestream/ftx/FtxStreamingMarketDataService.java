0482eb97db706500d9d4d8a732f548cca99d03d9
Liam Cordelle <liam.cordelle@mahifx.com>
2022-10-05 15:37:35 +1300

    FTX - Handle multiple subscriptions to orderbook

--- xchange-stream-ftx/src/main/java/info/bitrich/xchangestream/ftx/FtxStreamingMarketDataService.java
+++ xchange-stream-ftx/src/main/java/info/bitrich/xchangestream/ftx/FtxStreamingMarketDataService.java
@@ -1,8 +1,13 @@
 package info.bitrich.xchangestream.ftx;
 
+import com.google.common.cache.Cache;
+import com.google.common.cache.CacheBuilder;
 import com.google.common.collect.Lists;
 import info.bitrich.xchangestream.core.StreamingMarketDataService;
 import io.reactivex.Observable;
+import io.reactivex.disposables.CompositeDisposable;
+import io.reactivex.disposables.Disposable;
+import io.reactivex.subjects.PublishSubject;
 import org.knowm.xchange.currency.CurrencyPair;
 import org.knowm.xchange.dto.marketdata.OrderBook;
 import org.knowm.xchange.dto.marketdata.Ticker;
@@ -11,42 +16,65 @@ import org.knowm.xchange.ftx.FtxAdapters;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.Map;
+import java.util.concurrent.ExecutionException;
+
 public class FtxStreamingMarketDataService implements StreamingMarketDataService {
 
-  private static final Logger LOG = LoggerFactory.getLogger(FtxStreamingMarketDataService.class);
+    private static final Logger LOG = LoggerFactory.getLogger(FtxStreamingMarketDataService.class);
 
-  private final FtxStreamingService service;
+    private final FtxStreamingService service;
 
-  public FtxStreamingMarketDataService(FtxStreamingService service) {
-    this.service = service;
-  }
+    private final Cache<CurrencyPair, Observable<OrderBook>> orderBookByPair = CacheBuilder.newBuilder()
+            .softValues()
+            .build();
 
-  @Override
-  public Observable<OrderBook> getOrderBook(CurrencyPair currencyPair, Object... args) {
-    OrderBook orderBook = new OrderBook(null, Lists.newArrayList(), Lists.newArrayList());
-    String channelName = "orderbook:" + FtxAdapters.adaptCurrencyPairToFtxMarket(currencyPair);
+    public FtxStreamingMarketDataService(FtxStreamingService service) {
+        this.service = service;
+    }
 
-    return service
-        .subscribeChannel(channelName)
-        .map(
-            res -> {
-              try {
-                return FtxStreamingAdapters.adaptOrderbookMessage(orderBook, currencyPair, res);
-              } catch (IllegalStateException e) {
-                LOG.warn(
-                    "Resubscribing {} channel after adapter error {}",
-                    currencyPair,
-                    e.getMessage());
-                orderBook.getBids().clear();
-                orderBook.getAsks().clear();
-                // Resubscribe to the channel
-                this.service.sendMessage(service.getUnsubscribeMessage(channelName, args));
-                this.service.sendMessage(service.getSubscribeMessage(channelName, args));
-                return new OrderBook(null, Lists.newArrayList(), Lists.newArrayList(), false);
-              }
-            })
-        .filter(ob -> ob.getBids().size() > 0 && ob.getAsks().size() > 0);
-  }
+    @Override
+    public Observable<OrderBook> getOrderBook(CurrencyPair currencyPair, Object... args) {
+        OrderBook orderBook = new OrderBook(null, Lists.newArrayList(), Lists.newArrayList());
+        String channelName = "orderbook:" + FtxAdapters.adaptCurrencyPairToFtxMarket(currencyPair);
+
+        try {
+            return orderBookByPair.get(currencyPair,
+                    () -> {
+                        PublishSubject<OrderBook> pubsub = PublishSubject.create();
+
+                        Disposable sub = service
+                                .subscribeChannel(channelName)
+                                .map(
+                                        res -> {
+                                            try {
+                                                return FtxStreamingAdapters.adaptOrderbookMessage(orderBook, currencyPair, res);
+                                            } catch (IllegalStateException e) {
+                                                LOG.warn(
+                                                        "Resubscribing {} channel after adapter error {}",
+                                                        currencyPair,
+                                                        e.getMessage());
+                                                orderBook.getBids().clear();
+                                                orderBook.getAsks().clear();
+                                                // Resubscribe to the channel
+                                                this.service.sendMessage(service.getUnsubscribeMessage(channelName, args));
+                                                this.service.sendMessage(service.getSubscribeMessage(channelName, args));
+                                                return new OrderBook(null, Lists.newArrayList(), Lists.newArrayList(), false);
+                                            }
+                                        })
+                                .filter(ob -> ob.getBids().size() > 0 && ob.getAsks().size() > 0)
+                                .subscribe(pubsub::onNext);
+
+                        return pubsub.toSerialized().doOnDispose(() -> {
+                            sub.dispose();
+                            orderBookByPair.invalidate(currencyPair);
+                        });
+                    }
+            );
+        } catch (ExecutionException e) {
+            throw new RuntimeException(e);
+        }
+    }
 
   @Override
   public Observable<Ticker> getTicker(CurrencyPair currencyPair, Object... args) {
