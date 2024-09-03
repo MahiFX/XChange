76343432336ee714345a7d2a18826b6376230f14
mahijames <mahijames@users.noreply.github.com>
2024-09-03 15:22:51 +0100

    Fix enums

--- xchange-binance/src/main/java/org/knowm/xchange/binance/dto/trade/OrderType.java
+++ xchange-binance/src/main/java/org/knowm/xchange/binance/dto/trade/OrderType.java
@@ -1,7 +1,9 @@
 package org.knowm.xchange.binance.dto.trade;
 
 import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
 
+@JsonTypeInfo(use = JsonTypeInfo.Id.NONE)
 public enum OrderType {
   LIMIT,
   MARKET,
